c01df03e1c2aa1d3268d621537ae507be95cca59
OtitoU <136706167+Odera19@users.noreply.github.com>
2025-03-14 04:44:42 -0500

    update

--- ADvantage/app.py
+++ ADvantage/app.py
@@ -18,6 +18,13 @@ def handle_options_request():
     if request.method == "OPTIONS":
         return jsonify({"message": "OK"}), 200
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    return response
 
 
 @app.route("/")
